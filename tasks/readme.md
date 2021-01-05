815d06904f3834d4eda602f0ec5a5db612f331d7
sinzed <saeedzhd@gmail.com>
2021-01-05 13:29:40 +0100

    feat: add come in and go out

---
+++ tasks/readme.md
@@ -0,0 +1,25 @@
+## What is this
+    -Time is the most important thing what we have, however as a creative and not lazy person
+    you are not trying to spend your time adding your working hours in an old hr system called alinaweb
+    this would automate this task
+    
+## Installation
+    - clone the repository
+    - npm install
+    - copy config.example.js to config.js
+    - replace the right configurations of your account
+
+## Usage
+    - npm run worktime
+
+## Get Feedback
+
+- Feel free to give feedback
+
+## Contribute
+
+Contributions are always welcome!
+
+## License
+
+MIT license
--- tasks/worktimeIn.js
+++ tasks/worktimeIn.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 const config = require('../config/config')
 
 describe('add working hours not because you are lazy but because you have something more important to do', ()=>{
-    it('should add working hours correctingFilter', async function () {
+    it('should add working hours by correcting comeInFilter', async function () {
             const browser = await puppeteer.launch({
                 headless: config.headless,
                 ignoreHTTPSErrors: true,
@@ -22,27 +22,24 @@ describe('add working hours not because you are lazy but because you have someth
                 ]
               });
             const page = await browser.newPage()
-            await page.goto(config.url)
+            // await page.goto(config.url)
+            await page.goto("https://alinaweb/Webterminal/")
             await page.type('#txtUser', config.username)
             await page.type('#txtPassword', config.password)
             await page.click("#btnLogin")
             page.waitForTimeout(5000)
             await page.waitForNavigation()
-            const frame = await page.frames().find(frame => frame.name() === 'Stage');
+            const frame = await page.frames().find(frame => frame.name() === 'Upper');
             await frame.evaluate(() => {
-                const trs = Array.from(document.querySelectorAll('table#SectionGrid_Zeitkorrekturen1_SectionNA_DgNormabweichungen tbody tr'))
-                trs[0].children[0].children[0].click();
+                const kommenBtn = Array.from(document.querySelectorAll('a#Kommen'))
+                kommenBtn[0].click();
             })
             await page.waitForTimeout(2000)
-            await frame.evaluate(() => {
-              document.querySelector("#SectionGrid_Zeitkorrekturen1_SectionForm_MenuAktionen_Aktionen_SubAktionen_102").click()
+            const frameStage = await page.frames().find(frame => frame.name() === 'Stage');
+            await frameStage.evaluate(() => {
+                const okBtn = Array.from(document.querySelectorAll('a#Stempelung1_StempelSection_btnOk'))
+                okBtn[0].click();
             })
-            await page.waitForTimeout(2000)
-            await frame.type("#SectionGrid_Zeitkorrekturen1_SectionForm_TimeChooser_Von_TimeChooser_Von_TxtTime",config.workingFromTime)
-            await frame.type("#SectionGrid_Zeitkorrekturen1_SectionForm_TimeChooser_Bis_TimeChooser_Bis_TxtTime",config.workingTillTime)
-            await frame.click("#SectionGrid_Zeitkorrekturen1_SectionForm_btnOk")
-            await frame.type("#SectionGrid_Zeitkorrekturen1_SectionForm_RepZeitkontoEdit_ctl00_NumberEditVG_AutoCompleteTextBox","20")
-            // await frame.click("#SectionGrid_Zeitkorrekturen1_SectionForm_btnSpeichern")
-            // browser.close()
+            browser.close()
     })
 })
