8b9742f2b583f0de8bf7c9a339047396818e88e6
sinzed <saeedzhd@gmail.com>
2021-01-07 14:38:18 +0100

    feat: add cookie

--- config/config.example.js
+++ config/config.example.js
@@ -1,7 +1,12 @@
-var config = {}
+var config = {
+}
 config.workingFromTime = "09:30",
 config.workingTillTime = "18:00",
-config.username = "ZAHESAE",
-config.password = "ZAHESAE+1",
+config.email = "xxx@xxx.com",
+config.accountPass = "xxxxxx",
+config.username = "xxxxx",
+config.password = "xxxxx",
+config.externalUrl = "https://alinaweb.examplecompany.com/Webterminal/",
 config.url = "http://alinaweb/webterminal/aspx/MeineZeitkorrekturenListe.aspx",
+config.headless = false;
 module.exports = config;
--- tasks/worktimeInExtern.js
+++ tasks/worktimeInExtern.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs').promises;
 const config = require('../config/config')
 
 describe('add working hours not because you are lazy but because you have something more important to do', ()=>{
-    it('should add working hours by correcting goOutFilter', async function () {
+    it('should add working hours by correcting comeExternInFilter', async function () {
             const browser = await puppeteer.launch({
                 headless: config.headless,
                 ignoreHTTPSErrors: true,
@@ -22,16 +23,33 @@ describe('add working hours not because you are lazy but because you have someth
                 ]
               });
             const page = await browser.newPage()
-            // await page.goto(config.url)
-            await page.goto("https://alinaweb/Webterminal/")
+            try {
+              const cookiesString = await fs.readFile('./cookies.json');
+              const cookies = JSON.parse(cookiesString);
+              await page.setCookie(...cookies);
+            }
+            catch(ex){
+              console.warn(ex);
+              await page.goto(config.externalUrl)
+              // await page.waitForNavigation()
+              await page.waitForTimeout(2000)
+              await page.type('#i0116', config.email)
+              await page.click('#idSIButton9')
+              await page.waitForTimeout(2000)
+              await page.type('#i0118', config.accountPass)
+              await page.click('#idSIButton9')
+              await page.waitForTimeout(12000)
+              const cookies = await page.cookies();
+              await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
+            }
+            await page.goto(config.externalUrl)
             await page.type('#txtUser', config.username)
             await page.type('#txtPassword', config.password)
             await page.click("#btnLogin")
-            // await page.waitForTimeout(5000)
-            await page.waitForNavigation()
+            await page.waitForTimeout(3000)
             const frame = await page.frames().find(frame => frame.name() === 'Upper');
             await frame.evaluate(() => {
-                const kommenBtn = Array.from(document.querySelectorAll('a#Gehen'))
+                const kommenBtn = Array.from(document.querySelectorAll('a#Kommen'))
                 kommenBtn[0].click();
             })
             await page.waitForTimeout(2000)
@@ -40,6 +58,6 @@ describe('add working hours not because you are lazy but because you have someth
                 const okBtn = Array.from(document.querySelectorAll('a#Stempelung1_StempelSection_btnOk'))
                 okBtn[0].click();
             })
-            // browser.close(); 
+            browser.close()
     })
 })
